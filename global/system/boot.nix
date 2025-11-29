552b834d6687d6291a7e791878eb2a3558e30890
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-11-29 10:52:33 +0500

    boot loader

--- global/system/boot.nix
+++ global/system/boot.nix
@@ -1,8 +1,21 @@
 { ... }:
 
 {
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      grub2-theme = {
+        enable = true;
+        theme = "vimix";
+        footer = true;
+      };
+      grub = {
+        enable = true;
+        useOSProber = true;
+        efiSupport = true;
+        device = "nodev";
+      };
+      efi.canTouchEfiVariables = true;
+      efi.efiSysMountPoint = "/boot";
+    };
   };
 }
