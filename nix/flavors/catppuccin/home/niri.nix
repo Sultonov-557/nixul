d550a5928fc91568c686dd5d62a47c48a9edff79
Sultonov-557 <sultonovzerifboy@gmail.com>
2026-01-15 17:21:16 +0500

    fixed niri keybinds

--- nix/flavors/catppuccin/home/niri.nix
+++ nix/flavors/catppuccin/home/niri.nix
@@ -91,12 +91,18 @@
       "Mod+Shift+9".action.move-column-to-workspace = 9;
 
       # -- Media & System (Standard) --
-      "XF86AudioRaiseVolume".action.spawn =
-        [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+" ];
-      "XF86AudioLowerVolume".action.spawn =
-        [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-" ];
-      "XF86AudioMute".action.spawn =
-        [ "wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle" ];
+      "XF86AudioRaiseVolume".action = {
+        spawn = [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+" ];
+        allow-when-locked = true;
+      };
+      "XF86AudioLowerVolume".action = {
+        spawn = [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-" ];
+        allow-when-locked = true;
+      };
+      "XF86AudioMute".action = {
+        spawn = [ "wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle" ];
+        allow-when-locked = true;
+      };
       "Mod+Shift+M".action.spawn =
         [ "wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle" ];
 
@@ -106,15 +112,36 @@
       "Print".action.screenshot = [ ];
 
       # Media keys (playerctl)
-      "XF86AudioPlay".action.spawn = "playerctl play-pause";
-      "XF86AudioPause".action.spawn = "playerctl play-pause";
-      "XF86AudioNext".action.spawn = "playerctl next";
-      "XF86AudioPrev".action.spawn = "playerctl previous";
-      "XF86AudioStop".action.spawn = "playerctl stop";
+      "XF86AudioPlay".action = {
+        spawn = [ "playerctl" "play-pause" ];
+        allow-when-locked = true;
+      };
+      "XF86AudioPause".action = {
+        spawn = [ "playerctl" "play-pause" ];
+        allow-when-locked = true;
+      };
+      "XF86AudioNext".action = {
+        spawn = [ "playerctl" "next" ];
+        allow-when-locked = true;
+      };
+      "XF86AudioPrev".action = {
+        spawn = [ "playerctl" "previous" ];
+        allow-when-locked = true;
+      };
+      "XF86AudioStop".action = {
+        spawn = [ "playerctl" "stop" ];
+        allow-when-locked = true;
+      };
 
       # Brightness keys (brightnessctl)
-      "XF86MonBrightnessUp".action.spawn = "brightnessctl set +5%";
-      "XF86MonBrightnessDown".action.spawn = "brightnessctl set 5%-";
+      "XF86MonBrightnessUp".action = {
+        spawn = [ "brightnessctl" "set" "+5%" ];
+        allow-when-locked = true;
+      };
+      "XF86MonBrightnessDown".action = {
+        spawn = [ "brightnessctl" "set" "5%-" ];
+        allow-when-locked = true;
+      };
     };
 
     # Animations
