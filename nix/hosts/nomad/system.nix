f6f29af05c9edf942cee9bd55d8f80e2229e3795
Sultonov-557 <sultonovzerifboy@gmail.com>
2026-01-23 19:06:27 +0500

    added battery package

--- nix/hosts/nomad/system.nix
+++ nix/hosts/nomad/system.nix
@@ -1,9 +1,11 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [ ./hardware-configuration.nix ../../modules/system ];
 
   networking.hostName = "nomad";
+
+  environment.defaultPackages = with pkgs; [ acpi ];
 
   home-manager.users.${config.nixul.user}.imports = [ ./home.nix ];
 }
