3d1728f3b6b2cee3cbf9cbc7d551a6d050b70bec
Sultonov-557 <sultonovzerifboy@gmail.com>
2026-01-27 14:59:16 +0500

    firewall

--- nix/modules/system/hardware/connection/network.nix
+++ nix/modules/system/hardware/connection/network.nix
@@ -1,17 +1,10 @@
-{ ... }:
-{
+{ ... }: {
   # Networking
   networking.networkmanager.enable = true;
 
   # Open ports in the firewall.
   networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [
-    80
-    443
-  ];
-  networking.firewall.allowedUDPPorts = [
-    80
-    443
-  ];
+  networking.firewall.allowedTCPPorts = [ 80 443 3000 ];
+  networking.firewall.allowedUDPPorts = [ 80 443 3000 ];
 
 }
