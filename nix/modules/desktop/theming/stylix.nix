cccd6805359927c8115d7a5e9abb5878e2a7354a
Sultonov-557 <sultonovzerifboy@gmail.com>
2026-01-31 18:29:20 +0500

    generic theme (to be customized)

--- nix/modules/desktop/theming/stylix.nix
+++ nix/modules/desktop/theming/stylix.nix
@@ -1,11 +1,50 @@
-{ inputs, pkgs, ... }: {
+{ inputs, pkgs, config, ... }: {
 
   imports = [ inputs.stylix.nixosModules.stylix ];
+
+  home-manager.users.${config.nixul.user} = {
+    stylix.targets = {
+      nixvim = {
+        opacity.enable = true;
+        transparentBackground = {
+          main = true;
+          numberLine = true;
+          signColumn = true;
+        };
+      };
+    };
+  };
 
   stylix = {
     enable = true;
 
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-soft.yaml";
+    base16Scheme = {
+      scheme = "nixul";
+      author = "https://github.com/Sultonov-557/nixul";
+
+      # === Background shades ===
+      base00 = "#1e1e1e"; # Default background
+      base01 = "#181818"; # Darker background (panels, sidebars)
+      base02 = "#323232"; # Selection background
+      base03 = "#454545"; # Comments / subtle UI elements
+
+      # === Foreground shades ===
+      base04 = "#585858"; # Dark foreground (secondary text)
+      base05 = "#cdcdcd"; # Default foreground (main text)
+      base06 = "#f5f5f5"; # Light foreground (high emphasis)
+      base07 = "#ffffff"; # Bright foreground (rare highlights)
+
+      # === Accent colors ===
+      base08 = "#f38ba8"; # Red      (errors, danger)
+      base09 = "#fab387"; # Orange   (warnings, numbers)
+      base0A = "#f9e2af"; # Yellow   (attention, search)
+      base0B = "#a6e3a1"; # Green    (success)
+      base0C = "#94e2d5"; # Cyan     (info, hints)
+      base0D = "#89b4fa"; # Blue     (primary accent)
+      base0E = "#cba6f7"; # Purple   (secondary accent)
+      base0F = "#f2cdcd"; # Special  (rare / decorative)
+    };
+
     image = ../../../assets/wallpaper.png;
 
     polarity = "dark";
@@ -14,6 +53,13 @@
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Classic";
       size = 24;
+    };
+
+    opacity = {
+      applications = 0.0;
+      terminal = 0.0;
+      desktop = 0.0;
+      popups = 0.0;
     };
 
     fonts = {
@@ -29,6 +75,7 @@
         package = pkgs.inter;
         name = "Inter";
       };
+
       sizes = {
         applications = 11;
         terminal = 12;
