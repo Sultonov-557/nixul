c5d407ee090d5207a75c41bdc03c1de2c73c238c
Sultonov-557 <sultonovzerifboy@gmail.com>
2026-02-13 18:07:58 +0500

    feat(ollama): added ollama

--- nix/modules/apps/ai/opencode.nix
+++ nix/modules/apps/ai/opencode.nix
@@ -24,6 +24,19 @@
           question = "allow";
         };
         provider = {
+          ollama = {
+            npm = "@ai-sdk/openai-compatible";
+            name = "Ollama";
+            options = {
+              baseURL = "http://127.0.0.1:11434/v1";
+              num_ctx = "65536";
+            };
+            models = {
+              "qwen2.5-coder:0.5b" = {
+                name = "qwen2.5-coder:0.5b";
+              };
+            };
+          };
           google = {
             models = {
               "antigravity-gemini-3-pro" = {
