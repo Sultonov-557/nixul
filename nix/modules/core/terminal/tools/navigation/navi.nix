0ca0260f9fc80351d5d71c6004b2941f44397f80
sultonov <sultonovzerifboy@gmail.com>
2026-03-28 12:03:36 +0500

    feat: add navi cheatsheets

--- nix/modules/core/terminal/tools/navigation/navi.nix
+++ nix/modules/core/terminal/tools/navigation/navi.nix
@@ -10,6 +10,17 @@
     { cfg, ... }:
     {
       home.packages = lib.mkIf cfg.enable (with pkgs; [ navi ]);
+
+      xdg.configFile = lib.mkIf cfg.enable {
+        "navi/config.yaml".text = ''
+          cheatsheet: https://github.com/cheat/cheatsheets
+          cheatsheet: https://github.com/deniztraka/cheat_sh
+          cheatsheet: https://github.com/ibraheemdev/modern-unix
+          cheatsheet: https://github.com/sulami/navi-styles
+          cheatsheet: https://github.com/0xalof/navi-cheatsheet
+          cheatsheet: https://github.com/hartwork/navi-cheatsheet
+        '';
+      };
     };
 
   options = lib.mkOption {
--- todo.md
+++ todo.md
@@ -82,6 +82,7 @@
 - [x] feat: add doggo
 - [x] feat: add hyperfine
 - [x] feat: add proper providers to litellm
+- [x] feat: add navi cheatsheets
 
 - [x] test experimental features and merge with main branch
 
@@ -91,7 +92,6 @@
 - [ ] fix(zen): fix zen browser after github.com/0xc000022070/zen-browser-flake/issues/237 is resolved
 - [ ] fix(litellm): delete workaround after https://github.com/NixOS/nixpkgs/issues/432925 is resolved
 
-- [ ] feat: add navi cheatsheets
 - [ ] feat: add more bookmarks
 
 - [ ] feat: module integrations
