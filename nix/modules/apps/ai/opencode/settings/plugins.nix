9d2de1d94a984c9e52aacb836f33ee954e94c01a
sultonov <sultonovzerifboy@gmail.com>
2026-04-01 22:10:09 +0500

    feat: added more opencode plugins

--- nix/modules/apps/ai/opencode/settings/plugins.nix
+++ nix/modules/apps/ai/opencode/settings/plugins.nix
@@ -4,6 +4,13 @@
     "opencode-multi-openai-codex-auth"
     "opencode-agent-skills"
     "opencode-websearch"
+    "@nick-vi/opencode-type-inject"
+    "@tarquinen/opencode-dcp"
+    "@morphllm/opencode-morph-plugin"
+    "@zenobius/opencode-skillful"
+    "@plannotator/opencode"
+    "@spoons-and-mirrors/subtask2"
+    "octto"
     "opencode-memory"
     "opencode-git-context"
   ];
